be70918e51aeb9de88d4352842a3ce3018274bc9
iLukSbr <lucasyfm@hotmail.com>
2024-11-13 12:01:46 -0300

    test

--- esp32/main.py
+++ esp32/main.py
@@ -1,1 +1,16 @@
-# main.py -- put your code here!
+import machine
+import time
+
+# Configuração do pino do LED
+led = machine.Pin(2, machine.Pin.OUT)
+
+def blink_led():
+    led.on()
+    time.sleep(0.5)
+    led.off()
+    time.sleep(0.5)
+
+if __name__ == "__main__":
+    while True:
+        print("Hello, World!")
+        blink_led()
