ce0616b69e77df976d607b4255c39bc7bfd05e69
Letícia Walger Amaro <leticiaamaro@alunos.utfpr.edu.br>
2024-12-27 19:25:17 -0300

    Values are sent to app

--- getvalues.py
+++ getvalues.py
@@ -1,4 +1,9 @@
 from uart_comm_pi import UARTComm
+import socket
+import time
+
+HOST = ''  # Listen on all available interfaces
+PORT = 12345  # Port to listen on
 
 UARTComm_instance = UARTComm()
 
@@ -7,13 +12,29 @@ while True:
 
         message = UARTComm_instance.read_serial()
 
-        parts = message.split(",")
+        #parts = message.split(",")
 
-        co2 = parts[0]
-        temperature = parts[1]
-        humidity = parts[2]
-        nh3 = parts[3]
-        flame = parts[4]
+        #co2 = parts[0]
+        #temperature = parts[1]
+        #humidity = parts[2]
+        #nh3 = parts[3]
+        #flame = parts[4]
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind((HOST, PORT))
+            server_socket.listen()
+            print(f"Server listening on port {PORT}")
+    
+            conn, addr = server_socket.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    # Send data periodically
+
+                    conn.sendall(message.encode())
+                    print(f"Sent: {message}")
+            
+                    time.sleep(1)  # Wait 1 second before sending the next message
 
     except Exception as e:
-        print(f"Failed to process or get sensor data: {e}")
+        print(f"Failed to get sensor data: {e}")
