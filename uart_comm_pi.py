76797c30a433ed40fa1505aa4ddb1669aa3e2a72
iLukS <lucasyfm@hotmail.com>
2025-02-15 18:06:38 -0300

    uart buffer

--- uart_comm_pi.py
+++ uart_comm_pi.py
@@ -1,12 +1,15 @@
-import serial
+import serial, time
 
 class UARTComm:
-    BAUD_RATE = 115200
-    TIMEOUT = 1  # em segundos (para pySerial)
-
+    # BAUD_RATE = 115200
+    # TIMEOUT = 1  # em segundos (para pySerial)
+    BAUD_RATE = 9600
+    TIMEOUT = 5
+    
     def __init__(self, port='/dev/serial0', baudrate=BAUD_RATE, timeout=TIMEOUT):
         try:
-            self.uart = serial.Serial(port, baudrate, timeout=timeout)
+            self.uart = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
+            self.timeout = timeout
             print(f"UART initialized on port {port} with baudrate {baudrate}")
         except Exception as e:
             print(f"Failed to initialize UART: {e}")
@@ -19,11 +22,38 @@ class UARTComm:
             print(f"Failed to send message: {e}")
 
     def read_serial(self):
+        # try:
+        #     message = self.uart.readline().decode('utf-8').strip()
+        #     if message:
+        #         print(f"Received serial message: {message}")
+        #     return message
+        # except Exception as e:
+        #     print(f"Failed to read serial message: {e}")
+        #     return None
         try:
-            message = self.uart.readline().decode('utf-8').strip()
-            if message:
-                print(f"Received serial message: {message}")
-            return message
+            buffer = ""
+            start_time = time.ticks_ms()
+            
+            while True:
+                if self.uart.any():
+                    data = self.uart.read()
+                    if data:
+                        buffer += data.decode('utf-8')
+                        if '\n' in buffer:
+                            lines = buffer.split('\n')
+                            for line in lines[:-1]:
+                                line = line.strip()
+                                print(f"Received: {line}")
+                                return line
+                            buffer = lines[-1]
+                    start_time = time.ticks_ms()  # Reset the timeout timer
+                if time.ticks_ms() - start_time > self.timeout:
+                    if buffer:
+                        print(f"Received partial message: {buffer.strip()}")
+                        return None
+                    buffer = ""  # Clear the buffer after timeout
+                    start_time = time.ticks_ms()  # Reset the timeout timer
+                time.sleep_ms(100)
         except Exception as e:
-            print(f"Failed to read serial message: {e}")
+            print(f"Failed to read message: {e}")
             return None
