ba4e77932b2a44641b1996f46dcc65ccdd63fb90
iLukS <lucasyfm@hotmail.com>
2025-02-15 18:16:30 -0300

    uart name log

--- uart_comm_pi.py
+++ uart_comm_pi.py
@@ -17,18 +17,18 @@ class UARTComm:
     def send_message(self, message):
         try:
             self.uart.write((message + '\n').encode('utf-8'))
-            print(f"Sent message: {message}")
+            print(f"Sent UART message: {message}")
         except Exception as e:
-            print(f"Failed to send message: {e}")
+            print(f"Failed to send UART message: {e}")
 
     def read_serial(self):
         # try:
         #     message = self.uart.readline().decode('utf-8').strip()
         #     if message:
-        #         print(f"Received serial message: {message}")
+        #         print(f"Received UART message: {message}")
         #     return message
         # except Exception as e:
-        #     print(f"Failed to read serial message: {e}")
+        #     print(f"Failed to read UART message: {e}")
         #     return None
         try:
             start_time = time.perf_counter()
@@ -37,13 +37,13 @@ class UARTComm:
                 if self.uart.in_waiting:
                     message = self.uart.readline().decode('utf-8').strip()
                     if message:
-                        print(f"Received: {message}")
+                        print(f"Received UART message: {message}")
                         return message
                     start_time = time.perf_counter()  # Reset the timeout timer
                 if time.perf_counter() - start_time > self.timeout:
-                    print("Timeout reached without receiving a complete message.")
+                    print("Timeout reached without receiving a complete UART message.")
                     return None
                 time.sleep(0.1)
         except Exception as e:
-            print(f"Failed to read message: {e}")
+            print(f"Failed to read UART message: {e}")
             return None
