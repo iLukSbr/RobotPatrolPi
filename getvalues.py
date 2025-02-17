93a53f3c695977a4cff63648ae2df867ded18252
iLukSbr <lucasyfm@hotmail.com>
2025-02-17 20:22:32 -0300

    fix errno 98 address already in use

--- getvalues.py
+++ getvalues.py
@@ -18,6 +18,7 @@ while True:
         #flame = parts[4]
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             server_socket.bind((HOST, PORT))
             server_socket.listen()
             print(f"Server listening on port {PORT}")
